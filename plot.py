5735c91ba7a9d19dc1228dabec7241a20932f055
twenty5151 <tsuiyikching@protonmail.com>
2021-12-05 15:41:13 +0000

    Extract duplicated code in plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,25 @@
 from common import Constants
 import pandas as pd
+import matplotlib.pyplot as plt
 
+
+def core(outfile, plot_func, plot_format, plot_name, figsize):
+    from common import mkdirs_touch_open
+
+    if outfile.exists():
+        return
+
+    print(f'Plotting {plot_name} (offline)...')
+
+    plt.rcParams['font.family'] = 'Hiragino Sans GB'
+    plt.figure(figsize=figsize)
+
+    plot_func()
+    plot_format()
+
+    mkdirs_touch_open('', outfile)
+    plt.savefig(outfile)
+    plt.close()
 
 def format_int_time(tick_value: int, pos: int) -> str:
     '''
@@ -18,32 +37,25 @@ def seaborn_boxplot_combined(
     outfile: str,
 ):
     import seaborn as sns
-    import matplotlib.pyplot as plt
     from matplotlib.ticker import FuncFormatter
-    from common import mkdirs_touch_open
 
-    if outfile.exists():
-        return
+    def f():
+        # even if seaborn allowed datetimes, numpy couldn't add them for some reason
+        df.Arrive = df.Arrive.view(int) / 1e12
+        sns.boxplot(
+            data=df, x='Arrive', y='Station', hue='Line',
+            flierprops={'markersize': 1, 'linestyle': 'none'}
+        )
 
-    plt.rcParams['font.family'] = 'Hiragino Sans GB'
-    plt.figure(figsize=(15, 20))
+    def g():
+        plt.grid(which='both', alpha=0.7)
+        plt.gca().tick_params(axis='x', which='minor')
+        plt.gca().yaxis.set_tick_params(labelright='on')
+        plt.gca().xaxis.set_major_formatter(FuncFormatter(format_int_time))
+        plt.gca().xaxis.set_tick_params(labeltop='on')
+        plt.tight_layout()
 
-    # even if seaborn allowed datetimes, numpy couldn't add them for some reason
-    df.Arrive = df.Arrive.view(int) / 1e12
-    sns.boxplot(
-        data=df, x='Arrive', y='Station', hue='Line',
-        flierprops={'markersize': 1, 'linestyle': 'none'}
-    )
-
-    plt.grid(which='both', alpha=0.7)
-    plt.gca().tick_params(axis='x', which='minor')
-    plt.gca().yaxis.set_tick_params(labelright='on')
-    plt.gca().xaxis.set_major_formatter(FuncFormatter(format_int_time))
-    plt.gca().xaxis.set_tick_params(labeltop='on')
-    plt.tight_layout()
-    mkdirs_touch_open('', outfile)
-    plt.savefig(outfile)
-    plt.close()
+    return core(outfile, f, g, 'boxplot_combined', (15, 20))
 
 # only plots main branch
 def seaborn_boxplot(
@@ -52,98 +64,72 @@ def seaborn_boxplot(
     color: str,
 ):
     import seaborn as sns
-    import matplotlib.pyplot as plt
     from matplotlib.ticker import FuncFormatter
-    from common import mkdirs_touch_open
 
     outfile = Constants.plot_dir / f'{line_name}_{plot_name}.png'
-    if outfile.exists():
-        return
 
-    print(f'Plotting {plot_name} (offline)...')
+    def f():
+        # even if seaborn allowed datetimes, numpy couldn't add them for some reason
+        df.Arrive = df.Arrive.view(int) / 1e12
+        sns.boxplot(data=df, x='Arrive', y='Station', color=color)
+        # Gets too thin at the end (because too few stations), so boxplots are better
+        # This is why a ridgeline (https://seaborn.pydata.org/examples/kde_ridgeplot.html)
+        # plot won't work -- height of distribution too small
+        #sns.violinplot(data=df, x='Arrive', y='Station', color=color)
+        # This one needs change the division from 1e12 to 1e6
+        #sns.kdeplot(data=df, x="Arrive", hue='Station')
 
-    plt.rcParams['font.family'] = 'Hiragino Sans GB'
-    plt.figure(figsize=(15, 15))
+    def g():
+        plt.grid(which='both', alpha=0.7)
+        plt.gca().tick_params(axis='x', which='minor')
+        plt.gca().xaxis.set_major_formatter(FuncFormatter(format_int_time))
+        plt.gca().xaxis.set_tick_params(labeltop='on')
+        plt.gca().yaxis.set_tick_params(labelright='on')
+        plt.tight_layout()
 
-    # even if seaborn allowed datetimes, numpy couldn't add them for some reason
-    df.Arrive = df.Arrive.view(int) / 1e12
-    sns.boxplot(data=df, x='Arrive', y='Station', color=color)
-    # Gets too thin at the end (because too few stations), so boxplots are better
-    # This is why a ridgeline (https://seaborn.pydata.org/examples/kde_ridgeplot.html)
-    # plot won't work -- height of distribution too small
-    #sns.violinplot(data=df, x='Arrive', y='Station', color=color)
-    # This one needs change the division from 1e12 to 1e6
-    #sns.kdeplot(data=df, x="Arrive", hue='Station')
-
-    plt.grid(which='both', alpha=0.7)
-    plt.gca().tick_params(axis='x', which='minor')
-    plt.gca().xaxis.set_major_formatter(FuncFormatter(format_int_time))
-    plt.gca().xaxis.set_tick_params(labeltop='on')
-    plt.gca().yaxis.set_tick_params(labelright='on')
-    plt.tight_layout()
-    mkdirs_touch_open('', outfile)
-    plt.savefig(outfile)
-    plt.close()
+    return core(outfile, f, g, 'boxplot', (15, 15))
 
 def matplotlib(
     df: 'DataFrame', df_for_branch: 'Optional[DataFrame]',
     line_name: str, plot_name: str, alpha: float,
     color: str, line: bool,
 ):
-    import matplotlib.pyplot as plt
-    from matplotlib.dates import DateFormatter
-    from matplotlib.ticker import AutoMinorLocator
-    from common import mkdirs_touch_open
-
     outfile = Constants.plot_dir / f'{line_name}_{plot_name}.png'
-    if outfile.exists():
-        return
 
-    print(f'Plotting {plot_name} (offline)...')
-
-    plt.rcParams['font.family'] = 'Hiragino Sans GB'
-    plt.figure(figsize=(15, 15))
-
-    args = dict(
-        color=color,
-        alpha=alpha,
-        marker='o'
-    )
-    if not line:
-        args.update(dict(linestyle='None'))  # Must be a string!
-
-    # yikes
-    col = 'Station' if df_for_branch is None else 'Station_sequence'
-
-    for train in df.Train.unique():
-        plt.plot(
-            df[df.Train == train]['Arrive'],
-            df[df.Train == train][col],
-            **args
+    def f():
+        args = dict(
+            color=color,
+            alpha=alpha,
+            marker='o'
         )
+        if not line:
+            args.update(dict(linestyle='None'))  # Must be a string!
 
-    if df_for_branch is not None:
-        branch_color = 'tab:blue'
-        args['color'] = branch_color
-        for train in df_for_branch.Train.unique():
+        # yikes
+        col = 'Station' if df_for_branch is None else 'Station_sequence'
+
+        for train in df.Train.unique():
             plt.plot(
-                df_for_branch[df_for_branch.Train == train]['Arrive'],
-                df_for_branch[df_for_branch.Train == train][col],
+                df[df.Train == train]['Arrive'],
+                df[df.Train == train][col],
                 **args
             )
 
-    plt.grid(which='both', alpha=0.7)
-    plt.gca().tick_params(axis='x', which='minor')
-    plt.gca().invert_yaxis()
-    plt.gca().xaxis.set_minor_locator(AutoMinorLocator(2))
-    plt.gca().xaxis.set_major_formatter(DateFormatter('%H:%M'))
-    plt.gca().xaxis.set_minor_formatter(DateFormatter('%H:%M'))
-    plt.gca().xaxis.set_tick_params(labeltop='on')
-    plt.gca().yaxis.set_tick_params(labelright='on')
-    plt.tight_layout()
-    mkdirs_touch_open('', outfile)
-    plt.savefig(outfile)
-    plt.close()
+        if df_for_branch is not None:
+            branch_color = 'tab:blue'
+            args['color'] = branch_color
+            for train in df_for_branch.Train.unique():
+                plt.plot(
+                    df_for_branch[df_for_branch.Train == train]['Arrive'],
+                    df_for_branch[df_for_branch.Train == train][col],
+                    **args
+                )
+
+    def g():
+        format_mpl_plot(plt.gca())
+        plt.tight_layout()
+
+    return core(outfile, f, g, plot_name, (15, 15))
 
 def plot_ax_core(
     df: 'DataFrame',
@@ -151,7 +137,6 @@ def plot_ax_core(
     color: str, line: bool,
     ax: 'Optional[Axis]' = None
 ):
-    import matplotlib.pyplot as plt
 
     plt.rcParams['font.family'] = 'Hiragino Sans GB'
     if ax is None:
