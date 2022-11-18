4fce24c6d91a386b662356870556b2dcd4a6a420
akazukin5151 <tsuiyikching@protonmail.com>
2022-11-18 23:05:07 +0000

    Early return in app.simple.get_urls if result already exist

--- marey/app/simple/get_urls.py
+++ marey/app/simple/get_urls.py
@@ -2,11 +2,14 @@ from marey.lib import get_urls
 from .common import Constants, mkdirs_touch_open
 
 def main(line_name):
+    url_file = Constants.url_dir / f'{line_name}.txt'
+    url_dir = Constants.html_dir / line_name
+    if url_file.exists():
+        return
+
     rs = get_urls.main(
         html_path=Constants.html_dir / f'{line_name}.html',
     )
-    url_file = Constants.url_dir / f'{line_name}.txt'
-    url_dir = Constants.html_dir / line_name
 
     result = []
     for (target_url, train_dep_hour, train_dep_min, train_dest, train_speed) in rs:
