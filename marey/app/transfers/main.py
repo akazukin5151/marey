8bbbe99cef9996f41046beca712c90fc1f03669b
akazukin5151 <tsuiyikching@protonmail.com>
2022-11-19 13:42:29 +0000

    Improve variable names and comments in transfers app

--- marey/app/transfers/main.py
+++ marey/app/transfers/main.py
@@ -8,22 +8,27 @@ from marey.lib import prepare_plot
 from . import plot
 
 def main(route: Route):
-    outfile = Path('out/transfers/routing') / f'{route.filename}.html'
+    route_html_path = Path('out/transfers/routing') / f'{route.filename}.html'
     plot_dir = Path('out/transfers/plot')
-    plot_outfile = plot_dir / f'{route.filename}.png'
-    save_page.main(route.to_url(), outfile)
+    plot_out_path = plot_dir / f'{route.filename}.png'
+
+    # search the route on ekitan and save the result html
+    save_page.main(route.to_url(), route_html_path)
+
+    # scrape the route html
     all_stations_data = scrape_routing.scrape_html(route)
 
+    # for every leg of the journey... (the route is made of legs and transfers)
     dfs = []
     for (name, time, timetable_url) in all_stations_data:
         line_name = name + '_part'
 
         # download timetable url to html
-        outfile = Path('out/transfers/line') / f'{line_name}.html'
-        save_page.main(timetable_url, outfile)
+        timetable_html_path = Path('out/transfers/line') / f'{line_name}.html'
+        save_page.main(timetable_url, timetable_html_path)
 
         # scrape urls from html
-        rs = get_urls.main(outfile)
+        rs = get_urls.main(timetable_html_path)
 
         # find train that matches `time` and return url
         splitted = time.split(':')
@@ -35,19 +40,19 @@ def main(route: Route):
         else:  # no break
             raise Exception(f"couldn't find matching train at {time}")
 
-        # download that page
+        # download page of the train for this leg
         journey_html = Path('out/transfers/journey') / f'{name}-{time}.html'
         save_page.main(target_url, journey_html)
 
-        # scrape that html
+        # scrape html of train for this leg
         csv_path = Path('out/transfers/generated_csv') / (line_name + '.csv')
         scrap_html.main([journey_html], csv_path)
 
         # using new code, remove stations outside origin and destination
         # (outside specific time sections)
 
-        # prepare for plot
-        if plot_outfile.exists():
+        # prepare for plot (split up arrive and depart time into separate rows)
+        if plot_out_path.exists():
             continue
 
         processed_csv_path = csv_path.with_stem(line_name + '_processed')
@@ -57,8 +62,8 @@ def main(route: Route):
         )
         dfs.append(df)
 
-    # plot the result
-    plot.main(dfs, plot_outfile)
+    # plot the entire route
+    plot.main(dfs, plot_out_path)
 
 
 if __name__ == '__main__':
