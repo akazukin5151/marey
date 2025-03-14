1b5e772633d352b68cd2b285e401e5f87675c823
akazukin5151 <tsuiyikching@protonmail.com>
2025-03-15 02:45:14 +0800

    feat(transfer): plot multiple train with similar times

--- marey/app/transfers/main.py
+++ marey/app/transfers/main.py
@@ -47,17 +47,26 @@ def main(route: Route):
     # the route is made of legs and transfers
     # for every leg of the journey, scrape the leg
     dfs = []
-    for station_data, to_remove in zip(all_stations_data, names_to_remove):
+    all_colors = []
+    n_empty = 0
+    for idx, station_data in enumerate(all_stations_data):
+        to_remove = names_to_remove[idx]
+        color = colors[idx - n_empty]
         dfs_ = scrape_leg(station_data, to_remove, plot_out_path)
+
         dfs.extend(dfs_)
+        if len(dfs_) == 0:
+            n_empty += 1
+
+        all_colors.extend([color] * len(dfs_))
 
     # plot the entire route
-    plot.main(dfs, plot_out_path, colors, line_names)
+    plot.main(dfs, plot_out_path, all_colors, line_names)
 
 def scrape_leg(
     station_data: StationData,
     to_remove: str,
-    plot_out_path: Path
+    plot_out_path: Path,
 ) -> list[pd.DataFrame]:
     name = station_data[0]
     time = station_data[1]
@@ -79,7 +88,7 @@ def scrape_leg(
     splitted = time.split(':')
     hour = splitted[0]
     minute = splitted[1]
-    for (target_url, train_dep_hour, train_dep_min, _, _) in rs:
+    for idx, (target_url, train_dep_hour, train_dep_min, _, _) in enumerate(rs):
         if train_dep_hour == hour and train_dep_min == minute:
             break
     else:  # no break
@@ -88,18 +97,29 @@ def scrape_leg(
     dfs = []
 
     df = scrape_train(
-        name, time, line_name, plot_out_path, to_remove, target_url
+        name, time, plot_out_path, to_remove, target_url
     )
     if df is not None:
         dfs.append(df)
 
+    # plot many different times of the same line.
+    # based on the same timetable.
+    from_idx = max(idx - 10, 0)
+    to_idx = min(idx + 10, len(rs))
+    for r in rs[from_idx:to_idx]:
+        target_url = r[0]
+        time_ = r[1] + ':' + r[2]
+        df = scrape_train(
+            name, time_, plot_out_path, to_remove, target_url
+        )
+        if df is not None:
+            dfs.append(df)
 
     return dfs
 
 def scrape_train(
     name: str,
     time: str,
-    line_name: str,
     plot_out_path: Path,
     to_remove: str,
     target_url: str,
@@ -110,18 +130,23 @@ def scrape_train(
     save_page.main(target_url, journey_html)
 
     # scrape html of train for this leg
-    csv_path = Path('out/transfers/generated_csv') / (line_name + '.csv')
+    csv_path = Path('out/transfers/generated_csv') / f'{name}-{time}.csv'
     scrap_html.main([journey_html], csv_path)
 
     # prepare for plot (split up arrive and depart time into separate rows)
     if plot_out_path.exists():
         return None
 
-    processed_csv_path = csv_path.with_stem(line_name + '_processed')
+    processed_csv_path = csv_path.with_stem(csv_path.stem + '_processed')
     df = prepare_plot.prepare_normal(
         in_csv=csv_path,
         out_csv=processed_csv_path
     ).reset_index()
+
+    # TODO: this is because a transfer is required instead of through service.
+    # this can be supported later
+    if len(df[df['Station'] == to_remove]) == 0:
+        return None
 
     # remove stations outside origin and destination
     combined.remove_stations_after_exclusive(to_remove, df)
@@ -139,5 +164,11 @@ if __name__ == '__main__':
     main(Route(
         url='https://ekitan.com/transit/route/sf-2489/st-1483?sfname=%E7%94%B0%E7%84%A1&stname=%E7%A7%8B%E8%91%89%E5%8E%9F&dt=20250314&tm=1000',
         filename='田無→秋葉原',
+        result_idx=0,
+    ))
+
+    main(Route(
+        url='https://ekitan.com/transit/route/sf-5378/st-5212?sfname=%E7%A5%87%E5%9C%92%E5%9B%9B%E6%9D%A1&stname=%E8%BF%91%E9%89%84%E5%A5%88%E8%89%AF&dt=20250314&tm=0900',
+        filename='祇園四条→近鉄奈良',
         result_idx=0,
     ))
