dc30350e126fa48e1064ce98d8651abff28f8f40
akazukin5151 <tsuiyikching@protonmail.com>
2025-03-17 01:21:05 +0800

    feat: retry url fetch with exponential backoff

--- marey/lib/common.py
+++ marey/lib/common.py
@@ -1,8 +1,10 @@
 from typing import NamedTuple
+from time import sleep
 from datetime import date
 from datetime import datetime
 from pathlib import Path
 from urllib.request import urlopen
+from urllib.error import URLError
 from bs4 import BeautifulSoup
 
 class Constants(NamedTuple):
@@ -26,6 +28,17 @@ def mkdirs_touch_open(s: str, path: Path):
         f.write(s)
 
 def fetch_soup(url):
-    page = urlopen(url)
+    n_tries = 0
+    while True:
+        if n_tries > 0:
+            sleep(2 ** n_tries)
+        if n_tries >= 5:
+            raise Exception('failed to fetch')
+        try:
+            page = urlopen(url)
+            break
+        except URLError:
+            n_tries += 1
+
     html = page.read().decode("utf-8")
     return BeautifulSoup(html, "html.parser")
